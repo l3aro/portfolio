be63c36f4c0130c8a835bd32132a9e66891246b1
l3aro <dgbao1340@gmail.com>
2025-09-26 15:40:47 +0000

    home article

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,33 +1,30 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <x-seo::meta />
 
-    <x-seo::meta />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <style>
+            [x-cloak] {
+                display: none !important;
+            }
+        </style>
 
-    <style>
-        [x-cloak] {
-            display: none !important;
-        }
-    </style>
+        @fluxAppearance
+    </head>
 
-    @fluxAppearance
-</head>
-
-<body class="min-h-screen flex flex-col bg-white dark:bg-black max-w-7xl mx-auto">
-    <div class="flex flex-col grow">
-        <x-layouts.navigation />
-        <flux:main container
-            class="bg-zinc-50 dark:bg-zinc-900 ring-1 ring-zinc-200 dark:ring-zinc-300/20">
-            {{ $slot }}
-        </flux:main>
-    </div>
-    <x-layouts.footer />
-    @fluxScripts
-</body>
-
+    <body class="min-h-screen flex flex-col bg-white dark:bg-black max-w-7xl mx-auto">
+        <div class="flex flex-col grow">
+            <x-layouts.navigation />
+            <flux:main container class="bg-zinc-50 dark:bg-zinc-900 ring-1 ring-zinc-200 dark:ring-zinc-300/20">
+                {{ $slot }}
+            </flux:main>
+        </div>
+        <x-layouts.footer />
+        @fluxScripts
+    </body>
 </html>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,27 +1,48 @@
 <?php
 
+use App\Models\Article;
+use Livewire\Attributes\Computed;
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+    #[Computed]
+    public function articles()
+    {
+        return Article::query()
+            ->withWhereHas('url')
+            ->whereNotNull('published_at')
+            ->latest('published_at')
+            ->take(3)
+            ->get();
+    }
 }; ?>
 
 <div class="mt-9 w-full">
-    <div class="mx-auto max-w-2xl">
+    <div class="mx-auto max-w-3xl">
         <img src="{{ asset('images/avatar.jpg') }}" alt="Avatar" class="w-32 h-32 rounded-full mb-6 mx-auto" />
         <h1 class="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
             Software designer, founder, and amateur astronaut.
         </h1>
         <div class="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I'm Spencer, a software designer and entrepreneur based in New York
-            City. I'm the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I'm Spencer, a software designer and entrepreneur based in New York City. I'm the founder and CEO of
+            Planetaria, where we develop technologies that empower regular people to explore space on their own terms.
         </div>
         <div class="mt-6 flex gap-6">
             <flux:button href="https://github.com/l3aro" icon="github" target="blank"></flux:button>
-            <flux:button href="https://www.linkedin.com/in/bao-duong-924717186/" icon="linkedin" target="blank">
-            </flux:button>
+            <flux:button
+                href="https://www.linkedin.com/in/bao-duong-924717186/"
+                icon="linkedin"
+                target="blank"
+            ></flux:button>
+        </div>
+        <div class="mt-24 md:mt-28">
+            <div class="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+                <div class="flex flex-col gap-16">
+                    @foreach ($this->articles as $article)
+                        <x-home.article :$article />
+                    @endforeach
+                </div>
+            </div>
         </div>
     </div>
 </div>
