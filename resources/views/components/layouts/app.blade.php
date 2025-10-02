c8c211fa842e986b0046d999d69d92ccb083e19b
l3aro <dgbao1340@gmail.com>
2025-10-02 16:00:09 +0000

    reorder spotlight

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <x-layouts.base>
     @livewire('notifications')
-    @livewire('livewire-ui-spotlight')
     <div class="flex flex-col grow">
         <x-layouts.navigation />
         <flux:main container class="bg-zinc-50 dark:bg-zinc-900 ring-1 ring-zinc-200 dark:ring-zinc-300/20 pb-20!">
@@ -8,4 +7,5 @@
         </flux:main>
     </div>
     <x-layouts.footer />
+    @livewire('livewire-ui-spotlight')
 </x-layouts.base>
